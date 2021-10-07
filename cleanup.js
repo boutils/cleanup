4a39f34bcaf3649dcd3816ba123f6c8829b8f906
Francois Beaufils <francois@stoic.com>
2021-10-07 06:22:48 +0200

    Check SCSS sorting

--- cleanup.js
+++ cleanup.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Vuedoc = require('@vuedoc/parser');
 const { exec } = require('child_process');
+const { parse, stringify } = require('scss-parser');
 
 const DIRECTORY = './src';
 const DEFAULT_COLOR = '\x1b[0m';
@@ -29,6 +30,93 @@ function addInfo(file, lineNumber, type, message) {
 async function checker() {
   await checkVMCFiles();
   await checkVUEFiles();
+  await checkCSSFiles();
+}
+
+function findCSSBlockError(blocks) {
+  const errors = [];
+  for (const block of blocks) {
+    const error = getSortingError(
+      block.map((it) => it.value),
+      block[0].line + block.length
+    );
+    if (error) {
+      errors.push(error);
+    }
+  }
+
+  return errors;
+}
+
+function getSCSSBlocks(ast) {
+  if (ast.type !== 'block') {
+    const blocks = [];
+    for (const v of ast.value) {
+      if (typeof v === 'object') {
+        const subBlocks = getSCSSBlocks(v);
+        if (subBlocks.length > 0) {
+          blocks.push(...subBlocks);
+        }
+      }
+    }
+
+    return blocks;
+  }
+
+  const blocks = [];
+  const block = [];
+  for (const v of ast.value) {
+    if (v.type === 'rule') {
+      const subBlocks = getSCSSBlocks(v);
+      if (subBlocks.length > 0) {
+        blocks.push(...subBlocks);
+      }
+    }
+
+    if (v.type !== 'declaration') {
+      continue;
+    }
+
+    for (const declaration of v.value) {
+      if (declaration.type !== 'property') {
+        continue;
+      }
+
+      for (const d of declaration.value) {
+        if (d.type !== 'identifier') {
+          continue;
+        }
+
+        block.push({ line: d.start.line, value: d.value });
+      }
+    }
+  }
+
+  if (block.length > 0) {
+    blocks.push(block);
+  }
+
+  return blocks;
+}
+
+async function checkCSSFiles() {
+  const files = getFilesFromDirectory(DIRECTORY, '.scss');
+
+  for (const file of files) {
+    try {
+      const data = fs.readFileSync(file, { encoding: 'utf8', flag: 'r' });
+      const ast = parse(data);
+      const blocks = getSCSSBlocks(ast);
+      const sortingErrors = findCSSBlockError(blocks);
+      if (sortingErrors) {
+        for (sortingError of sortingErrors) {
+          addWarning(file, sortingError.lineNumber, 'sorting', sortingError.message);
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
 
 async function checkVMCFiles() {
