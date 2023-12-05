40dbdb6cdbdfee743d22ee0afcfd2d6f70102b55
Francois Beaufils <francois@stoic.com>
2023-12-05 06:07:23 +0100

    Update cleanup.js

--- cleanup.js
+++ cleanup.js
@@ -1515,7 +1515,11 @@ async function checkVUEFiles() {
         addWarning(file, lineNumber, 'space', `'=' should be surronded by at least one space`);
       }
 
-      if (lineInfo.eventName?.endsWith('ed') && !lineInfo.eventName?.endsWith(':focused')) {
+      if (
+        lineInfo.eventName?.endsWith('ed') &&
+        !lineInfo.eventName?.endsWith(':focused') &&
+        !lineInfo.eventName?.startsWith('node')
+      ) {
         addWarning(file, lineNumber, 'past participle', `"${lineInfo.eventName}" should not end with 'ed'`);
       }
 
