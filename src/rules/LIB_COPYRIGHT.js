11f6f20beafc8f54d749ce3b6f0bb794ec398897
Francois Beaufils <francoisbeaufils@gmail.com>
2024-10-17 17:28:27 +0200

    Update LIB_COPYRIGHT.js

--- src/rules/LIB_COPYRIGHT.js
+++ src/rules/LIB_COPYRIGHT.js
@@ -8,11 +8,20 @@ export default {
     for (const filePath of filesPaths) {
       const { lines } = index.byPath[filePath];
       const firstLine = lines[0];
+      const secondLine = lines[1];
       if (firstLine && String(firstLine).trim() !== String(COPYRIGHT).trim()) {
         errors.push({
           filePath,
           line: 1,
           message: 'Missing or invalid copyright',
+        });
+      }
+
+      if (secondLine !== '') {
+        errors.push({
+          filePath,
+          line: 2,
+          message: 'Add an empty line after copyright',
         });
       }
     }
