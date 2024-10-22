f9c228e392828c174b251244d7bd6e73a6c7f6f4
Francois Beaufils <francoisbeaufils@gmail.com>
2024-10-22 10:46:51 +0100

    Update HTML_MISSING_EMPTY_LINE.js

--- src/rules/HTML_MISSING_EMPTY_LINE.js
+++ src/rules/HTML_MISSING_EMPTY_LINE.js
@@ -13,7 +13,6 @@ export default {
           !lineInfo.isEmptyLine &&
           lineInfo.hasStartingTag &&
           !previousLineInfo.allowMultipleTags &&
-          !lineInfo.isClosingTag &&
           lineInfo.lineNumber - 1 > 0 &&
           previousLineInfo.hasEndingTag
         ) {
