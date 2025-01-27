5b7f3153f2f7507e13c8405cd1848f5bc71bab72
Francois Beaufils <beaufils@sutoiku.com>
2025-01-27 06:54:32 +0100

    Create CSS_EMPTY_LINE.js

---
+++ src/rules/CSS_EMPTY_LINE.js
@@ -0,0 +1,39 @@
+export default {
+  validate: (index) => {
+    const filesPaths = index.byType['style'];
+
+    const errors = [];
+    for (const filePath of filesPaths) {
+      const { content } = index.byPath[filePath];
+
+      let isInside = false;
+      const lines = content.split('\n');
+      for (const [i, line] of lines.entries()) {
+        const previousLine = lines[i + 1];
+        const nextLine = lines[i + 1];
+        if (
+          line.trim() === '' &&
+          !previousLine?.includes('--') &&
+          isInside &&
+          nextLine &&
+          !nextLine?.includes('{') &&
+          !nextLine?.includes(',')
+        ) {
+          errors.push({
+            filePath: filePath,
+            line: i + 1,
+            message: `Remove this empty line`,
+          });
+        }
+
+        if (line.includes('{') && !line.includes('}')) {
+          isInside = true;
+        } else if (line.includes('}')) {
+          isInside = false;
+        }
+      }
+    }
+
+    return { errors };
+  },
+};
