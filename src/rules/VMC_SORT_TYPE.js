3be0b69c861a2206192980564d8af7000237cdcf
Francois Beaufils <francoisbeaufils@gmail.com>
2025-03-05 09:03:57 +0100

    Update VMC_SORT_TYPE.js

--- src/rules/VMC_SORT_TYPE.js
+++ src/rules/VMC_SORT_TYPE.js
@@ -33,7 +33,7 @@ export default {
           isIgnored = false;
         }
 
-        if (trimmedLine.startsWith('type ')) {
+        if (trimmedLine.startsWith('type ') && !trimmedLine.includes('};')) {
           isInsideType = true;
           relativeLineNumber[typeIndex] = lineIndex + 1;
         } else if (isInsideType && trimmedLine.includes('{') && !trimmedLine.includes('}')) {
