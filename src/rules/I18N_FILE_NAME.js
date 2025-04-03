85fb17cdb1f172d4e7e2e688e7cfbfd6a06cc470
Francois Beaufils <beaufils@sutoiku.com>
2025-04-03 06:19:41 +0200

    Create I18N_FILE_NAME.js

---
+++ src/rules/I18N_FILE_NAME.js
@@ -0,0 +1,19 @@
+export default {
+  validate: (index) => {
+    const filesPaths = index.byType['json'];
+
+    const errors = [];
+    for (const filePath of filesPaths) {
+      const { content } = index.byPath[filePath];
+
+      if (content.includes('"en":') && !filePath.endsWith('.i18n.json')) {
+        errors.push({
+          filePath,
+          message: 'File contains i18n but is not named *.i18n.json',
+        });
+      }
+    }
+
+    return { errors };
+  },
+};
