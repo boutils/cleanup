81cd243b37af0a4072cd9913da2b7a44890f404d
Francois Beaufils <beaufils@sutoiku.com>
2025-06-03 04:41:13 +0200

    Update TERMS.js

--- src/rules/TERMS.js
+++ src/rules/TERMS.js
@@ -1,20 +1,33 @@
 const TERMS_PATH = 'libs/typescript/components/shared/theme/terms.ts';
 export default {
   validate: (index) => {
-    const { lines } = index.byPath[TERMS_PATH];
+    const termFile = index.byPath[TERMS_PATH];
 
     const terms = {};
-    for (const line of lines) {
-      if (line.includes('{ icon: ')) {
-        const split = line.split(':');
-        const term = split[0].replaceAll("'", '').trim();
-        const icon = split[split.length - 1].replaceAll("'", '').replaceAll(',', '').replaceAll('}', '').trim();
-        terms[icon] ??= [];
-        terms[icon].push(term);
+
+    if (termFile) {
+      for (const line of termFile.lines) {
+        if (line.includes('{ icon: ')) {
+          const split = line.split(':');
+          const term = split[0].replaceAll("'", '').trim();
+          const icon = split[split.length - 1].replaceAll("'", '').replaceAll(',', '').replaceAll('}', '').trim();
+          terms[icon] ??= [];
+          terms[icon].push(term);
+        }
       }
     }
 
     const errors = [];
+
+    if (Object.keys(terms).length === 0) {
+      errors.push({
+        filePath: TERMS_PATH,
+        line: null,
+        message: 'Terms file is empty or does not contain any terms with icons.',
+      });
+      return { errors };
+    }
+
     for (const [icon, term] of Object.entries(terms)) {
       if (term.length > 1) {
         errors.push({
