f4a0c226041f1c0983d4f0f69512ad191245665f
Francois Beaufils <beaufils@sutoiku.com>
2025-09-19 04:04:17 +0200

    Update LIB_UNUSED_ASYNC.js

--- src/rules/LIB_UNUSED_ASYNC.js
+++ src/rules/LIB_UNUSED_ASYNC.js
@@ -1,4 +1,10 @@
-const IGNORED = [];
+const IGNORED = [
+  'src/libs/signals-store.ts__getOrFetchSignals',
+  'src/libs/worker/libs/instruments.worker.ts__searchInstruments',
+  'src/libs/worker/libs/intraday-data.worker.ts__fetchIntradayData',
+  'src/libs/worker/libs/regimes-events-schema.worker.ts__fetchRegimesSchema',
+  'src/libs/worker/libs/ticker-relations.worker.ts__computeTickerRelations',
+];
 
 export default {
   validate: (index) => {
